94137b61d3629a41191d5a35fa6e1f21efdad528
Manaswitha Saripalli <saripalli.manaswitha31@gmail.com>
2024-02-04 21:31:06 +0530

    message

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@
 import './App.css';
 import Home from './components/Home';
 import Chat from './components/Chat';
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return(
       <>
-        <HashRouter>
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
           <Route path="chat" element={<Chat />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
       </>
   );
   
