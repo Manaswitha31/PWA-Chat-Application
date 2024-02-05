ed52b82aae75583ff9148f8e7b2d83f88c9878cd
Manaswitha Saripalli <saripalli.manaswitha31@gmail.com>
2024-02-05 15:46:23 +0530

    message

--- src/components/Recording.js
+++ src/components/Recording.js
@@ -4,12 +4,12 @@ import React from "react";
 function Recording(){
     return( 
         <>
-            <div class="container">
-                <div class="display">
+            <div className="container">
+                <div className="display">
 
                 </div>
 
-                <div class="controllers">
+                <div className="controllers">
 
                 </div>
             </div>
